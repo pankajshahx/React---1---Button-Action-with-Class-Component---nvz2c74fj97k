350216a15f9f70e451e4bc329d59e9ff5315e219
Shubham Gupta <50530172+Shubham6697@users.noreply.github.com>
2021-11-05 13:45:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,25 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
-
+    constructor() {
+        super();
+        this.state ={
+          data : "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy",
+          flag : false
+        }
+	  };
+    setData(){
+      this.setState({flag:true})
+    }
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+        { /* Do not remove this main div!! */ }
+        {
+            this.state.flag ? <p id = "para" > {this.state.data} </p> : null
+        }
+        
+        <button onClick = {()=>this.setData()} > Click Me </button>
     		</div>
     	);
     }
@@ -17,4 +28,3 @@ class App extends Component {
 
 
 export default App;
-
